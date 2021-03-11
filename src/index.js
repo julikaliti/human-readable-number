9868412966f9067616084ac426842120cfe3bab6
julikaliti <yulikaliti@gmail.com>
2021-03-11 22:08:03 +0300

    feat: add function converts given number to readable string

--- src/index.js
+++ src/index.js
@@ -1,3 +1,41 @@
 module.exports = function toReadable (number) {
-  
+    if (number === 0) {return 'zero';}
+    
+    if (number > 0 && number <= 19) {
+      return units(number);
+    }
+    
+    if (number > 19 && number <= 99) {
+      return tenth(number);
+    }
+    
+    if (number >= 100 && number <= 999) {
+      var str = units(Math.floor(number/100)) + ' hundred';
+      if (number%100 == 0) {
+        return str;
+      } else if ((number%100 > 0) && (number%100 <=19)) {
+          str += ' ' + units(number%100);
+          return str;
+      } else {
+        str += ' ' + tenth(number%100);
+        return str;
+      }
+    }
 }
+  
+function units(number) {
+    let a =[, 'one', 'two', 'three', 'four', 'five', 'six', 'seven','eight', 'nine', 'ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen'];
+    unit = a[number];
+    return unit;
+}
+  
+function tenth(number) {
+  let b =[, , 'twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty','ninety'];
+  if (number%10 !== 0) {
+    tens = b[Math.floor(number/10)] + ' ' + units(number%10);
+    } else {
+      tens = b[Math.floor(number/10)];
+  }
+  return tens;			
+  }
+  
